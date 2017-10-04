42665d0bf07244bef68f15b91f7db02123b9d904
the_z <the_z@znel.org>
2017-10-04 14:57:13 +0100

    Add server model

---
+++ server.py
@@ -0,0 +1,54 @@
+import requests
+import re
+
+class Server():
+   
+    def __init__(self, name, address, username, password):
+        self.name = name
+        self.address = address
+        self.username = username
+        self.password = password
+
+        self.session = self.new_session()
+        self.motd = self.load_motd()
+
+        self.game = {
+            'players_max': 6,
+            'map': 'kf-default',
+            'round': 0,
+            'length': 7,
+            'difficulty':'normal'
+        }
+
+        self.players = {}
+
+    def new_session(self):
+        login_url = "http://" + self.address + "/ServerAdmin/"
+        login_payload = {
+            'password_hash': self.password,
+            'username': self.username,
+            'remember': '-1'
+        }
+
+        s = requests.Session()
+
+        login_page_response = s.get(login_url)
+
+        token_ex = "token\" value=\"(.*)\" \/"
+        mo = re.search(token_ex, login_page_response.text)
+        if mo:
+            login_payload.update({'token':mo.group(1)})
+
+
+        login_page_response = s.get(login_url)
+        s.post(login_url, data=login_payload)
+        
+        return s
+        
+    def load_motd(self):
+        motd_f = open(self.name + ".motd")
+        motd = motd_f.read()
+        motd_f.close()
+
+        return motd.encode("iso-8859-1", "ignore")
+
