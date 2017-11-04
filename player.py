18787e899c991d907b3fee86d6f9fb3f754a7336
the_z <the_z@znel.org>
2017-11-04 16:21:12 +0000

    Implement watchdog and death events

--- player.py
+++ player.py
@@ -5,14 +5,21 @@ class Player():
         self.total_kills = 0
         self.total_time = 0
         self.total_dosh = 0
+        self.total_dosh_spent = 0
+        self.total_health_lost = 0
         self.total_logins = 0
         self.last_login = 0
 
         self.session_kills = 0
         self.session_start_time = 0
         self.session_dosh = 0
+        self.session_dosh_spent = 0
+        self.session_health_lost = 0
 
         self.dosh = dosh
+        self.dosh_spent = 0
+        self.wave_kills = 0
+        self.health_lost_wave = 0
         self.kills = kills
         self.health = health
         self.username = username
