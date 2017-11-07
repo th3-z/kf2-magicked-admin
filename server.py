d777fbf979a5b457e91a70ad77a991cc351b4508
the_z <the_z@znel.org>
2017-11-07 11:14:12 +0000

    Add banner and link support to MotdUpdater

--- server.py
+++ server.py
@@ -188,25 +188,6 @@ class Server():
     def set_maplist(self, maplist):
         pass
 
-    def submit_motd(self, motd):
-        motd_url = "http://173.199.74.63:23002/ServerAdmin/settings/welcome"
-
-        motd_payload = {
-            'BannerLink': '',
-            'ClanMotto': '',
-            'ClanMottoColor': '#FF0000',
-            'ServerMOTD': motd.encode("iso-8859-1", "ignore"),
-            'ServerMOTDColor': '#FF0000',
-            'WebLink': '',
-            'WebLinkColor': '#FF0000',
-            'liveAdjust': '1',
-            'action': 'save'
-        }
-
-        print("INFO: Updating motd")
-        self.session.post(motd_url, data=motd_payload)
-
-
     def close(self):
         print("Terminating mapper thread...")
         self.mapper.terminate()
