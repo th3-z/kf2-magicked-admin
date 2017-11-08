2ff97ca2a230f1875780f16e117e436e50ac0cc6
the_z <the_z@znel.org>
2017-11-08 01:12:50 +0000

    Move some functions to utils.py

---
+++ utils.py
@@ -0,0 +1,14 @@
+import math
+
+def millify(n):
+    millnames = ['','K','M','B','T']
+        
+    n = float(n)
+    millidx = max(0,min(len(millnames)-1,
+        int(math.floor(0 if n == 0 else math.log10(abs(n))/3))))
+
+    return '{:.0f}{}'.format(n / 10**(3 * millidx), millnames[millidx])
+
+def trim_string(input_str, length):
+    return (input_str[:length-2] + '..') if len(input_str) > length else input_str
+
