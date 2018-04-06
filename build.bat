2b747021c98958259685cd18cfdeb62562983630
WilliamBrown42 <williamjacobbrown42@gmail.com>
2018-04-06 08:08:03 -0500

    Removed some lines to deal with CD spam
    Also more comment notes.

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 cd magicked_admin
-python -m pip install cx_freeze requests lxml configparser colorama termcolor
+python -m pip install cx_freeze requests lxml configparser colorama termcolor re
 python setup.py build -b ../bin
 cd ..
 pause
