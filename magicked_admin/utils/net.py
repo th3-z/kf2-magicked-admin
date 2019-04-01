dd5b41f7f3fb2b73cee539a3466a649893f2a2ae
the_z <the_z@znel.org>
2019-04-01 15:53:37 +0000

    Add address validation utils

---
+++ magicked_admin/utils/net.py
@@ -0,0 +1,18 @@
+from urllib.request import urlopen
+
+def repair_address_scheme(address):
+    address = address.strip()
+
+    if len(address) < 9 or "://" not in address[:8]:
+        address = "http://" + address
+
+    return address
+
+def is_valid_address(address):
+    try:
+        code = urlopen(address).getcode()
+    except:
+        return false
+
+    return code < 400
+
