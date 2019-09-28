6ace19cb24aef3fd61d162e14f6b125712c2bf1a
the_z <the_z@znel.org>
2019-09-28 14:34:10 +0100

    Add util tests

---
+++ tests/magicked_admin/utils/test_time.py
@@ -0,0 +1,20 @@
+import pytest
+
+from magicked_admin.utils.time import seconds_to_hhmmss
+
+
+def test_seconds_to_hhmmss():
+    tests = [
+        {"seconds": 12,
+         "expected": "00:00:12"},
+        {"seconds": 0,
+         "expected": "00:00:00"},
+        {"seconds": 60 * 60 * 3,
+         "expected": "03:00:00"},
+        {"seconds": 71237934,
+         "expected": "19788:18:54"}
+    ]
+
+    for test in tests:
+        result = seconds_to_hhmmss(test["seconds"])
+        assert result == test["expected"]
