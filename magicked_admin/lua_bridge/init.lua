7709ff8757ed632617a376c0192ece5b53971101
the_z <the_z@znel.org>
2019-10-23 21:44:17 +0100

    More lua binds

--- magicked_admin/lua_bridge/init.lua
+++ magicked_admin/lua_bridge/init.lua
@@ -3,10 +3,14 @@ function log(mesg)
 end
 
 -- Called by python to add binds to namespaces
-function bind_py_func(namespace, py_func)
-    log("Creating Python bind - " .. namespace .. "." .. py_func.__name__)
-    name = py_func.__name__
-    _G[namespace][name] = py_func
+function bind_py_func(namespace, func_name, py_func)
+    log(
+        "Creating Python bind - "
+            .. namespace .. "."
+            .. func_name .. " -> "
+            .. py_func.__name__
+    )
+    _G[namespace][func_name] = py_func
     return bind_py_func
 end
 
@@ -14,9 +18,4 @@ end
 function new_namespace(namespace)
     log("Creating namespace - " .. namespace)
     _G[namespace] = {}
-end
-
-function bind_test()
-    log("Entered bind_test in init.lua")
-    lua_bridge.bind_test()
 end
--- magicked_admin/lua_bridge/lua_bridge.py
+++ magicked_admin/lua_bridge/lua_bridge.py
@@ -3,6 +3,7 @@ from lupa import LuaRuntime
 
 from web_admin.chat import ChatListener
 from utils.lua import load_script
+from utils import find_data_file
 
 
 class LuaBridge(ChatListener):
@@ -23,17 +24,37 @@ class LuaBridge(ChatListener):
         self.create_binds()
 
     def create_binds(self):
+        commands = self.chatbot.commands
+
         self.new_namespace("chat")
-        self.new_bind("chat", self.say)
+        self.new_bind(
+            "chat", "say", self.chatbot.chat.submit_message
+        )
+
+        self.new_namespace("hooks")
+        # ...
+
+        self.new_namespace("server")
+        self.new_bind(
+            "server", "write_players", self.server.write_all_players
+        )
+        self.new_bind(
+            "server", "write_game", self.server.write_game_map
+        )
+        self.new_bind(
+            "server", "get_player", self.server.get_player_by_sid
+        )
+        self.new_bind(
+            "server", "get_players", lambda: self.server.players
+        )
+
+        self.new_namespace("motd")
 
     def execute_script(self, filename):
         self.lua.execute(load_script(filename))
 
     def execute(self, string):
         self.lua.execute(string)
-
-    def say(self, message):
-        self.chatbot.chat.submit_message(message)
 
     def receive_message(self, username, message, user_flags):
         # TODO: Call lua event handler
