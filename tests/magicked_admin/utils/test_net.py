c209a3eb0c3e15e0ec73a005c1a941d485fa60d3
th3-z <the_z@znel.org>
2020-02-03 18:15:37 +0000

    Lint

--- tests/magicked_admin/utils/test_net.py
+++ tests/magicked_admin/utils/test_net.py
@@ -2,6 +2,7 @@ import pytest
 
 from magicked_admin.utils.net import get_country, phone_home, resolve_address
 
+
 def test_get_country():
     expected = ("United States", "US")
     unknown = ("Unknown", "??")
