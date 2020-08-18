0eaf78a65a50e81fcd66973c9b4f64ecf2273810
the_z <the_z@znel.org>
2020-08-18 06:19:13 +0100

    Move player queries into model

--- magicked_admin/database/__init__.py
+++ magicked_admin/database/__init__.py
@@ -13,17 +13,19 @@ def _dict_row_factory(c, r):
     dict([(col[0], r[idx]) for idx, col in enumerate(c.description)])
 
 
+# TODO: move to database.py and import here
 def db_connector(func):
-    def with_connection_(*args, **kwargs):
+    def _with_connection(*args, **kwargs):
         conn = sqlite3.connect(
             _sqlite_db_file,
             check_same_thread=False,
             isolation_level=None,
         )
         conn.row_factory = _dict_row_factory
+        kwargs['conn'] = conn
 
         try:
-            ret = func(conn, *args, **kwargs)
+            ret = func(*args, **kwargs)
         except Exception:
             conn.rollback()
             warning(_("SQLite connection error"))
@@ -34,9 +36,10 @@ def db_connector(func):
             conn.close()
 
         return ret
-    return with_connection_
+    return _with_connection
 
 
+# TODO: as above
 def db_init():
     if not path.exists(_sqlite_db_file):
         info(_("Building new database..."))
